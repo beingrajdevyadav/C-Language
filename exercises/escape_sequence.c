80c61fd7dbf881815c69e06b070d44e4139f60ca
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-14 22:50:44 +0530

    Escape Sequence Topic Completed.

---
+++ exercises/escape_sequence.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Print a string with escape sequences
+    printf("Hello, World!\n");
+    printf("This is a tab:\tTabbed text\n");
+    printf("This is a backslash: \\\n");
+    printf("This is a double quote: \"\n");
+    return 0;
+}
+// Output:
