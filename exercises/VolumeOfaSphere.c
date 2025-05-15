b32e8094cb8f8e5ea51ba03a697a5cad14da8dbe
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-15 16:15:55 +0530

    Volume Of A Sphere Program Completed.

---
+++ exercises/VolumeOfaSphere.c
@@ -0,0 +1,17 @@
+// Write a program to calculate the Valume of a Sphere
+
+// Let's Focus Float Data Type
+
+#include <stdio.h>
+int main(){
+float c = 4;
+float t = 3;
+float pi = 3.14;
+float r;
+r = 5;
+float valume = (c*pi*r*r*r)/t;
+
+printf("%f", valume);
+
+return 0;
+}
