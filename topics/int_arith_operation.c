4b863f80cc9686ba8b63506285a57aba4b8253da
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-15 15:57:16 +0530

    Int Data Type Maths Operations Done.

---
+++ topics/int_arith_operation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int x = 5;
+    int y = 2;
+
+    printf("%d", x + y);
+    printf("\n%d", x - y);
+    printf("\n%d", x * y);
+    printf("\n%d", x / y);
+
+    return 0;
+}
