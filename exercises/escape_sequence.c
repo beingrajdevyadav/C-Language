0d5b117f2a7c4bbd07c191973c1ffdb319e6614e
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-17 23:16:18 +0530

    Updated

--- exercises/escape_sequence.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Print a string with escape sequences
-    printf("nn\n\nnn\n");
-    printf("nn/n/nnn/n");
-    return 0;
-}
-// Output: 
---
+++ input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+// printf("Hello World");
+int a, b;
+
+printf("Enter a : ");
+scanf("%d", &a);
+
+printf( " a = %d" , a);
+    return 0;
+}
