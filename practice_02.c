2eb1ae488a7c043e56f6e443d2b1aae7416d2d3b
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-17 23:40:02 +0530

    Practice 02 Done.

---
+++ practice_02.c
@@ -0,0 +1,15 @@
+// Write A Program To Calculate Surface Area Of Sphere
+
+#include <stdio.h>
+int main(){
+float pi = 3.1415;
+float r;
+
+printf("Enter Radius Value = ");
+scanf("%f", &r);
+
+float A = 4 * pi * r * r;
+printf("Sphere Surface Area = %f", A);
+    return 0;
+
+}
