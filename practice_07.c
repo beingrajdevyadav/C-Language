3ba6cc7e2ef9c9254ee2547b156bd38daa93c650
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-18 22:05:24 +0530

    Practice 07 Done.

---
+++ practice_07.c
@@ -0,0 +1,15 @@
+// Cube Volume
+
+#include <stdio.h>
+int main(){
+float pi = 3.1415;
+float a, V;
+
+printf("Enter Value Of a = ");
+scanf("%f", &a);
+
+V = a * a * a;
+
+printf("Volume Of Cube = %f", V);
+    return 0;
+}
