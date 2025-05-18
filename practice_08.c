9051744ff30792b93980ae473b2bed211c98c7cb
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-18 22:12:50 +0530

    Practice 08 Done.

---
+++ practice_08.c
@@ -0,0 +1,17 @@
+// Cube Surface Area
+
+#include <stdio.h>
+
+int main(){
+float a, A;
+
+printf("Enter Value Of a = ");
+scanf("%f", &a);
+
+A = 6 * a * a;
+
+printf("Surface Area Of Cube = %f", A);
+
+
+return 0;
+}
