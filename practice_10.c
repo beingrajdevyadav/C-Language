199a0304f949fb9674d1e1318efb4bf51fd986a5
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-23 22:01:47 +0530

    Practice 10 Done.

---
+++ practice_10.c
@@ -0,0 +1,14 @@
+// reminder, modulas
+
+#include <stdio.h>
+int main(){
+int a, b,c ,d;
+
+a = 2 % 5;
+b = -2 % 5;
+c = 2 % (-5);
+d = -2 % (-5);
+
+printf("a = %d b = %d c = %d d = %d", a, b, c, d);
+    return 0;
+}
