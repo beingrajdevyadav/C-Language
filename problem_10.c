f9e881e0f8c422445e3544b4531a00b57a55c260
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-25 23:59:48 +0530

    Problem 10 Attempted.

---
+++ problem_10.c
@@ -0,0 +1,35 @@
+// Check whether a triangle is valid or not
+
+
+#include <stdio.h>
+
+int main(){
+int side1, side2, side3, largeside, sum;
+printf("\n Enter three sides of the triangle : ");
+scanf("%d %d %d", side1, side2, side3);
+
+if(side1 > side2){
+    if(side1 > side3){
+        sum = side2 + side3;
+        largeside = side1;
+    }else{
+        sum = side1 + side2;
+        largeside = side3;
+    }
+}else {
+    if(side2 > side3){
+sum = side1 + side3;
+largeside = side2;
+    }else{
+        sum = side1 + side2;
+        largeside = side3;
+    }
+}
+
+if(sum > largeside){
+    printf("the triangle is a valid.");
+}else{
+    printf("the triangle is not a valid.");
+}
+    return 0;
+}
